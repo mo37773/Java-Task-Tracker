3386b31ea74286aaf8f257a7eb62b2b5bbbba812
martin <x24528929@student.ncirl.ie>
2026-03-23 17:50:52 +0000

    2nd Commit, update feature added and also a while loop that keeps app running until exit is entered, allows to test update feature

--- JavaTaskTracker/src/javatasktracker/JavaTaskTracker.java
+++ JavaTaskTracker/src/javatasktracker/JavaTaskTracker.java
@@ -13,39 +13,86 @@ import java.util.Scanner;
  * @author marti
  */
 public class JavaTaskTracker {
-     
 
-
-   
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        ArrayList<Task> toDoList = new ArrayList<>();       
+        //need to add while loop to keep app running unti i type exit
+        ArrayList<Task> toDoList = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         //after declaring the array list and scanner I will accept an input
-        System.out.println("Enter a command 'add/update/delete/mark/list");
+        System.out.println("Enter a command 'add/update/delete/mark/list'");
         String input = sc.nextLine();
-        //if the input is add I'll prompt the user to ask 
-        if(input.equalsIgnoreCase("add")){
-            System.out.println("Type the task you would like to add and hit enter...");
-            String taskDescription = sc.nextLine();
-            int taskId = toDoList.size() + 1;
-            String taskStatus = "to-do";
-            LocalDateTime taskCreatedAt = LocalDateTime.now();
-            LocalDateTime taskUpdatedAt = LocalDateTime.now();
-            Task newTask = new Task(taskId,taskDescription, taskStatus, taskCreatedAt, taskUpdatedAt);
-            toDoList.add(newTask);
-            System.out.println("Task has been added successfully!");          
+        while (!input.equalsIgnoreCase("exit")) {
+
+            //if the input is add I'll prompt the user to ask 
+            if (input.equalsIgnoreCase("add")) {
+                System.out.println("Type the task you would like to add and hit enter...");
+                String taskDescription = sc.nextLine();
+                //because the arraylist first index is 0 and we are adding to end I will use the size which is 0 now and add 1 so the first id starts at 1
+                int taskId = toDoList.size() + 1;
+                String taskStatus = "to-do";
+                //localdate time because it looks better than currentTimeMillis
+                LocalDateTime taskCreatedAt = LocalDateTime.now();
+                LocalDateTime taskUpdatedAt = LocalDateTime.now();
+                //create new object and add it to my to do list with confirmation message
+                Task newTask = new Task(taskId, taskDescription, taskStatus, taskCreatedAt, taskUpdatedAt);
+                toDoList.add(newTask);
+                System.out.println("Task has been added successfully!");
+            } else if (input.equalsIgnoreCase("update")) {
+                if (toDoList.isEmpty()) {
+                    System.out.println("The To Do List is empty!!");
+                } else {
+                    System.out.println("Enter the id of the task you want to update..");
+                    int taskId = sc.nextInt();
+                    /*DEBUGGINGthis is to fix the debugging issue I commented below regarding updatedDesc, testing now 
+                        ***TEST SUCCESS****
+                    */
+                    sc.nextLine();
+                    boolean taskFound = false;
+                    //get the task object from the array list if the id exists (we will traverse the array for this) and then set a new description 
+                    for (int i = 0; i < toDoList.size(); i++) {
+                        //make a task object for the item we get at i to make it easier
+                        Task retrievedTask = toDoList.get(i);
+                        // if the task get id matches the user inputed id then this logic runs
+                        if (retrievedTask.getId() == taskId) {
+                            System.out.println("TASK FOUND IN LIST, the next line you type will modify the task.");
+                            /*
+                            DEBUGGING: LINE 62 is being SKIPPED, turns out it is from sc.nextInt() line 49, I will need to add a new 
+                            sc.nextLine() after to fix the input
+                            ****FIXED*****
+                            */
+                            String updatedDesc = sc.nextLine();
+                            //we update the description and the updated time
+                            retrievedTask.setDescription(updatedDesc);
+                            retrievedTask.setUpdatedAt(LocalDateTime.now());
+                            //confirmation message
+                            System.out.println("Task with ID: " + retrievedTask.getId() + " updated successfully!!\n New Task: " + retrievedTask.getDescription() + "\n Updated At: " + retrievedTask.getUpdatedAt());
+                            //the boolean value is used incase we do not find the id in the arraylist
+                            taskFound = true;
+                            //break out of the loop
+                            break;
+                        }
+                    }
+                    //if the id is not found in the list we will display a message accordingly
+                    if (taskFound == false) {
+                        System.out.println("The id you have entered does not exist in the list!");
+                    }
+                    /*MAYBE A WHILE LOOP HERE 
+                    int i = 0;
+                    boolean taskFound = false;
+                    while(!taskFound && i < toDoList.size(){
+                    
+                *
+                     */
+
+                }
+            }
+            System.out.println("Enter 'add/update/delete/mark/list' or 'exit' to close");
+            input = sc.nextLine();
         }
-        
-        System.out.println(toDoList.get(0).toString());
-        
-        
-        
-        
     }
-    
-    
+
 }
